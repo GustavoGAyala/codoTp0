24f47075e4cfd37f73e28dc5958e776a769c7d07
Gustavo Ayala <gustavoayala25@gmail.com>
2023-05-12 14:55:37 -0300

    consulta de tasas al banco europeo mediante API rest

--- js/index.js
+++ js/index.js
@@ -1,8 +1,16 @@
 
+/*
 const URL = "https://musicbrainz.org/ws/2/."
 fetch(URL)
   .then(res => res.json())
   .then(data => {
     const audio = document.querySelector('audio');
     audio.src = data[0].url;
+  });
+  */
+const host = 'https://api.frankfurter.app/latest?from=USD';
+fetch(host)
+  .then(resp => resp.json())
+  .then((data) => {
+    console.log(data);
   });
---
+++ js/productos.js
@@ -0,0 +1,26 @@
+
+    
+const host = 'https://api.frankfurter.app/currencies';
+var dato = "";
+    fetch(host)
+      .then(resp => resp.json())
+      .then((data) => {
+        dato = data;
+      });
+const host2 = 'https://api.frankfurter.app/latest';
+var dato2 = "";
+    fetch(host2)
+        .then(resp => resp.json())
+        .then((data) => {
+            dato2 = data;
+            });
+function agregartxt1(){
+    var text = document.createTextNode(JSON.stringify(dato));  
+    
+    document.getElementById("boxTasas").appendChild(text);
+    var text2 = document.createTextNode(JSON.stringify(dato2))
+    document.getElementById("boxTasas2").appendChild(text2)
+               
+                                      
+     
+    }
